49860408493dc3f85e8c4a7f9751a4854d7499b9
StlMaris123 <stellamarisnjage@gmail.com>
2020-03-09 18:23:52 +0200

    wire up the Provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
     return [
         {title: 'Hello', duration: 2.30 },
@@ -13,3 +15,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
     };
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
