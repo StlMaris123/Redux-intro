279a3c910d1ed5deae2cdf632bba913d02b2037c
StlMaris123 <stellamarisnjage@gmail.com>
2020-03-09 15:45:17 +0200

    buildibg reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        {title: 'Hello', duration: 2.30 },
+        {title: 'Perfect', duration: 4.30 },
+        {title: 'Ngai Murathimi', duration: 3.55 },
+        {title: 'Twilight', duration: 4.20 }
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if action.type === 'SONG_SELECTED' {
+        return action.payload        
+    };
+    return selectedSong;
+};
